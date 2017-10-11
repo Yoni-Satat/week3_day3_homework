862b3c74cae2b17fc138ea0a5e34055fa82d5fe8
Yoni Satat <yoni.satat@gmail.com>
2017-10-11 17:59:19 +0100

    added save and show_all methods to artists.rb

--- music_app/models/artists.rb
+++ music_app/models/artists.rb
@@ -1,5 +1,6 @@
 require('pg')
 require_relative('albums.rb')
+require_relative('../db/sql_runner.rb')
 
 class Artist
   attr_reader :name, :id
@@ -8,6 +9,24 @@ class Artist
     @id = options['id'].to_i if options['id']
     @name = options['name']
   end
+
+  def save
+      sql = "
+        INSERT INTO artists(name)
+        VALUES($1)
+        RETURNING id
+      "
+      values = [@name]
+      @id = SqlRunner.run(sql, values)[0]['id'].to_i()
+    end
+
+    def self.show_all()
+        sql = "SELECT * FROM artists"
+        values = []
+        artists = SqlRunner.run(sql, values)
+        artist = artists.map{|artist| Artist.new(artist)}
+        return artist
+    end
 
 
 end
